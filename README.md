ade6daff8e7040a9578726cc49c6ccdb2f3175e3
Dylan Hubble <hubbledylan@gmail.com>
2019-02-09 01:14:07 +0000

    Added better comments

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Counts trees by looking at high quality drone imagery using deep neural networks
 
 Uses MlpClassifier for neural network
 
-trained on 200 25*25 pixel images of trees
+
+trained on 200 25*25 pixel images of trees then classifys every pixel in main image.
+
+To run your self
+1. Place 50 pictures of trees that are 25*25 pixels in the "Good" directory
+2. Place 150 pictures of things that are 25*25 pixels and not trees in the "Bad" directory
+3. Place all images to be queued up in the ready directory
+
 
 ![1](1.png)
+
 
--- classify2.py
+++ classify2.py
@@ -1,14 +1,26 @@
 from PIL import Image
 from sklearn.neural_network import MLPClassifier
-from skimage import feature
-from skimage import io
-import numpy as np
-import scipy
-from scipy import ndimage
 import glob
-import time
-#clf = MLPClassifier(solver = 'lbfgs' , alpha = 1e-5, hidden_layer_sizes(5,2), random_state 
-from sklearn.ensemble import RandomForestClassifier
+
+
+
+
+
+
+good_directory = "/home/hubbledylan/good/*.png"
+bad_directory = "/home/hubbledylan/bad/*.png"
+
+good_count = 50
+bad_count = 150
+
+#used to speed up processing by only classifiy a portion of base images
+#1 = 1/1 size, 2 = 1/4 size, 3 = 1/9 size ...
+scale = 1
+
+
+
+
+
 
 def process(location):
     base = Image.open(i).convert("RGB")
@@ -20,47 +32,56 @@ def process(location):
             temp += (vals[0]/256.0,vals[1]/256.0,vals[2]/256.0)
     return [temp]
 
-total = []
 
+
+total = []
 #open and read all "good" pictures
-locations = glob.glob("/home/hubbledylan/good/*.png")
+locations = glob.glob(good_directory)
 for i in locations:
     total += process(i)
 
 #open and read all "bad" pictures
-locations = glob.glob("/home/hubbledylan/bad/*.png")
+locations = glob.glob(bad_directory)
 for i in locations:
     total += process(i)
 
-#currently set up with 50 "good" and 150 "bad"
-Y = [1 for i in range(50)]
-Y += [0 for i in range(150)]
+#Matches Y values to images
+Y = [1 for i in range(good_count)] + [0 for i in range(bad_count)
 
 
+
+#sets up and fits neural net
 clf = MLPClassifier(solver = 'lbfgs' , alpha = 1e-5, hidden_layer_sizes = (250,100,40), random_state = 1)
 
 print "fitting..."
 
 clf.fit(total ,Y)
+
 print "Complete"
+
+
+#builds list of all png/jpg to be processed
 ready = glob.glob("/home/hubbledylan/ready/*.jpg")
 ready.extend(glob.glob("/home/hubbledylan/ready/*.png"))
-count = 0
+
+#iterates through all files to be processed
 for i in ready:
-    print i
-    base =Image.open(i)
+    #loads image
+    base = Image.open(i)
     base = base.convert('RGB')
     pixels = base.load()
 
+    #makes a new blank image
     output = Image.new('RGB',(base.size[0],base.size[1]))
     pixel = output.load()
-    temp = []
-    count1 = 0
-    for i in range((base.size[0]-27)/1):
-        for j in range((base.size[1]-27)/1):
-            tempi = i*1
-            tempj = j*1
+
+    #goes through all pixels, adjusted by "scale"
+    for i in range((base.size[0]-25)/scale):
+        for j in range((base.size[1]-25)/scale):
+            tempi = i*scale
+            tempj = j*scale
             
+            #cuts out a 25*25 pixel box to the left and down from current pixel location
             temp = []
             for x in range(25):
                 for y in range(25):
@@ -69,16 +90,16 @@ for i in ready:
             total = [temp]
             
 
+            #classifys cut out image and highlights depending on classifaction
             if clf.predict(total)[0] == 0:
                 if pixel[i,j] != (255,255,255):
                     pixel[i,j] =  pixels[tempi,tempj]
             else:
                 if pixel[i,j] != (255,255,255):
                     pixel[i,j] = pixels[tempi,tempj]
-                    pixel[i+12,j+12] = (255,255,255)
-        
+                    pixel[i+int(scale/2),j+int(scale/12)] = (255,255,255)
+        #prints progress through image
         print str(i) + " of " + str(base.size[0]/1)
-        if i%20 == 0:
-            output.save("/home/hubbledylan/3.png")
-    output.save("/home/hubbledylan/" + str(i) + ".png")
+    #saves each image
+    output.save("/home/hubbledylan/" + i + ".png")
             
